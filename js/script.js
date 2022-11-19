9a5a0e8763b7d08455d934336a75e610ab5bc363
Vlad <vladxaoc11@gmail.com>
2022-11-19 13:17:24 +0200

    films QA

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-let number = 5;
-const leftBorderWidth = 1;
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+for (let index = 0; index < 2; index++) {
+	let movie = prompt("Один из последних просмотренных фильмов?");
+	let star = prompt("На сколько оцените его?");
+	personalMovieDB["movies"][movie] = star;
+}
+
+console.log(personalMovieDB);
