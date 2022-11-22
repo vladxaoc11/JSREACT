9cdc8ea2982191cdcf7c3e05e93e6418b9229260
Vlad <vladxaoc11@gmail.com>
2022-11-22 21:22:49 +0200

    genres

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+	while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+	}
+}
+
+start();
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -7,10 +16,58 @@ const personalMovieDB = {
 	genres: [],
 	privat: false,
 };
-for (let index = 0; index < 2; index++) {
-	let movie = prompt("Один из последних просмотренных фильмов?");
-	let star = prompt("На сколько оцените его?");
-	personalMovieDB["movies"][movie] = star;
+
+function rememberMyFilms() {
+	for (let index = 0; index < 2; index++) {
+		let movie = prompt("Один из последних просмотренных фильмов?");
+		let star = prompt("На сколько оцените его?");
+		if (
+			movie != null &&
+			star != null &&
+			movie != "" &&
+			star != "" &&
+			movie.length < 50
+		) {
+			personalMovieDB["movies"][movie] = star;
+			console.log("done");
+		} else {
+			console.log("error");
+			index--;
+		}
+	}
 }
 
-console.log(personalMovieDB);
+rememberMyFilms();
+
+function detectPersonalLevel() {
+	if (personalMovieDB.count < 10) {
+		console.log("Просмотрено довольно мало фильмов");
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log("Вы классный зритель");
+	} else if (personalMovieDB.count >= 30) {
+		console.log("Вы киноман");
+	} else {
+		console.log("Произошла ошибка");
+	}
+}
+
+detectPersonalLevel();
+
+function showMyDB() {
+	if (personalMovieDB.privat == false) {
+		console.log(personalMovieDB);
+	}
+}
+
+function writeYourGenres() {
+	for (let index = 1; index <= 3; index++) {
+		let genres = prompt(`Ваш любисый жанр под номером ${index}`);
+		if (genres != null && genres != "" && genres.length < 50) {
+			personalMovieDB["genres"].push(genres);
+			console.log("done");
+		} else {
+			console.log("error");
+			index--;
+		}
+	}
+}
